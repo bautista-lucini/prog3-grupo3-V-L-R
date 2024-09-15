8d80b4b2fa0c58dbd9d28fc72c8c140bf90bc793
prog2-grupo2-RosellaValenciaSchwarzmaier <rosellac@udesa.edu.ar>
2024-09-15 18:40:18 -0300

    trate de hacer en el home el formulario de busqueda

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import "./Home.css"
+import FormularioBusqueda from '../../components/FormularioBusqueda/FormularioBusqueda'
 
 const Home = () => {
   return (
     <>
       <h1> Películas </h1>
+      <main>
+        <FormularioBusqueda/>
+      </main>
     </>
   )
 }
