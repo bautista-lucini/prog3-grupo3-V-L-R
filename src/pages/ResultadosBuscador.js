28c111e8b4c894db00c3eab81a3113735e36421a
Victoria Valencia <mvalencia@udesa.edu.ar>
2024-09-17 17:54:44 -0300

    terminando el resultado de busqueda.

--- src/pages/ResultadosBuscador.js
+++ src/pages/ResultadosBuscador.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react'
+import MovieCard from "../components/MovieCard/MovieCard";
 
 export class ResultadosBuscador extends Component {
   
     constructor(props){
         super(props)
+        this.state = {
+            resultado : [],
+        }
         
     }
     componentDidMount(){
         //fetch
         fetch ("compeltar")
-        .then()
-        .then()
-        .catch()
+            .then((response) => response.json())
+            .then((data) => this.setState({resultado: data.results, cargando: false}))
+            .catch((error) => console.log(error))
     }
   
     render() {
         return (
-            <div> Resultados de Busqueda: {this.props.location.state.query}</div>
+            <>
+               <h2> Resultados de Busqueda: {this.props.location.state.query}</h2>
+               {
+                    this.state.resultado.length === 0 ?
+                        <p> No se encontraron resultados </p> :
+                        this.state.resultado.map((unaPeli, idx) => <MovieCard key={unaPeli.name + idx} datos={unaPeli}/>)
+                }
+            </>
     )
   }
 }
